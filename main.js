127f0d4733e2be3b2ecf0e1b4dbcc580513b8945
danilo marichal <danilo.marichal@icloud.com>
2017-03-13 17:30:23 -0400

    add div stars and aircraft

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log('working!')
+
+
+var rock = $('<div></div>');
+rock.addClass('rock')
+$('body').append(rock);
+
+var stars = $('<div></div>');
+stars.addClass('stars')
+$('body').append(stars);
+
+
+
+
